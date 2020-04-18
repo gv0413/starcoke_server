d5d2b69a3b53b9243bdc7d62ce752f96813f5e13
gv0413 <ggvv0413@gmail.com>
2020-04-19 01:08:34 +0900

    계정 등록 및 로그인 API 추가

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const flash = require('express-flash')
 const session = require('express-session')
 const methodOverride = require('method-override')
 const initializePassport =  require('./passport-config')
+const jwt = require('jsonwebtoken');
 initializePassport(
   passport, 
   email => users.find(user => user.email === email),
@@ -17,11 +18,9 @@ initializePassport(
 )
 
 app.use(express.json())
-// app.set('view-engine', 'ejs')
-// app.use(express.urlencoded({ extended: false }))
 app.use(flash())
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: process.env.SESSION_SECRET || 'tmpSecretKey',
   resave: false,
   saveUninitialized: false
 }))
@@ -29,54 +28,90 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(methodOverride('_method'))
 
-// app.get('/', checkAuthenticated, (req, res) =>{
-//   res.render('index.ejs', {name : req.user.name })
-// })
+let users = [];
+const defaultConfig = {
+  chainId: '5300575914426995782',
+  mt: {
+    symbol: 'FT9754',
+  },
+  st: {
+    symbol: 'DST01',
+  },
+  walletAddress: {
+    pd: '',
+    user: ''
+  },
+  dapp: {
+    apiKey: ''
+  },
+  txActionName: {
+    like: 'like',
+    funding: 'funding',
+    purchase: 'purchase',
+    getOwner: 'getOwner',
+    setOwner: 'setOwner'
+  },
+  userName: '',
+}
+const dynamicConfig = [
+  {
+    apiKey: 'G2auRtGRzre2GoXXGZLYzZqawfLfBi2Prn9miWW65iRQTyLe9ASeicUYwbKVD416',
+    user: '0x3e877a621a56d7785f3525f542f1adc5dcaeb3c2',
+    pd: '0x773f69537fd1a47fdd9adae3a1181daaa4d3b168',
+  },
+  {
+    apiKey: 'hLM6r3oQ7jyGW1VoLra3ciy6ZczKJVTLGQgZSbzPYLM4UvZWytjcdBJBAqX6w2E3',
+    user: '0x5c2b51025301da8893c3fbb0075ac42756f83273',
+    pd: '0x54b41d3dbbae365c8bea4b4ddcf8efd279e68625',
+  },
+  {
+    apiKey: 'eF3hNeRVXFwZ1YuhDfQHd8V2ngkK5pocrA3MJyBx8bX2EENRb9ZfRT9N63TNMQGv',
+    user: '0x7b2f92579cd00657e929bee55822780d3e5fd39d',
+    pd: '0x84bb88215f71587435cc4c0cd4f29e4c1ae2af5b',
+  },
+];
 
-app.get('/dino', (req, res) =>{
-  // res.send({
-  //   result: true,
-  //   data: 'dino',
-  // })
-  res.send(true)
-})
-
-// app.get('/login',checkNotAuthenticated, (req, res)=> {
-//   res.render('login.ejs')
-// })
-
-// app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-//   successRedirect: '/',
-//   failureRedirect: '/login',
-//   failureFlash: true
-// }))
-
-// app.get('/register', checkNotAuthenticated, (req, res)=> {
-//   res.render('register.ejs')
-// })
-
-// app.post('/register', checkNotAuthenticated, async (req, res)=> {
-//   try{
-//     const hashedPassword = await bcrypt.hash(req.body.password, 10)
-//     users.push({
-//       id: Date.now().toString(),
-//       name: req.body.name,
-//       email: req.body.email,
-//       password: hashedPassword
-//     })
-//     res.redirect('/login')
-//   } catch {
-//     res.redirect('/register')
-//   }
-//   console.log(users)
-// })
-
-app.post('/login', (req, res)=> {
-  const user = users.find(e => e.email === req.body.email)
-  if( user && user.password === req.body.password){
+app.post('/register', async (req, res)=> {
+  try{
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: users.length,
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
     res.send({
       res: true,
-      data: 'Login success!',
+      data: 'Register success!',
+    })
+  } catch {
+    res.status(400)
+    .send({
+      res: false,
+      data: 'Register failed',
+    })
+  }
+  console.log(users)
+})
+
+app.post('/login', async (req, res)=> {
+  const user = users.find(e => e.email === req.body.email)
+  if( user && await bcrypt.compare(req.body.password, user.password)){
+    // create jwt token
+    defaultConfig.walletAddress.user = dynamicConfig[user.id  % 3].user;
+    defaultConfig.walletAddress.pd = dynamicConfig[user.id  % 3].pd;
+    defaultConfig.dapp.apiKey = dynamicConfig[user.id  % 3].apiKey;
+    defaultConfig.userName = user.name;
+    const token = jwt.sign({
+      email: user.email,
+      name: user.name,
+      config: defaultConfig,
+    }, process.env.ENCRYPT_KEY);
+    res.send({
+      res: true,
+      data: {
+        jwtToken: token,
+      },
     })
   }
   else{
@@ -87,45 +122,6 @@ app.post('/login', (req, res)=> {
   }
 })
 
-let users = []
-
-app.post('/register', (req, res) => {
-  res.send({
-    res: true,
-    data: 'Register success!',
-  })
-
-  if (!users.find(e => e.name === req.body.name)) {
-    users.push({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-  }
-
-  console.log(`users = ${JSON.stringify(users)}`)
-})
-
-app.delete('/logout', (req, res) => {
-  req.logOut()
-  res.redirect('/login')
-})
-
-// function checkAuthenticated(req, res, next)  {
-//   if(req.isAuthenticated()) {
-//     return next()
-//   }
-
-//   res.redirect('/login')
-// }
-
-// function checkNotAuthenticated(req, res, next) {
-//   if(req.isAuthenticated()){
-//     res.redirect('/')
-//   }
-//   next()
-// }
 app.listen(3000, () => {
   console.log(`http://localhost:3000`);
 })
-

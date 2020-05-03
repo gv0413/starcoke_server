1dbb9fb4727df5f6c383635f1ed797b351f0792e
gv0413 <ggvv0413@gmail.com>
2020-05-03 19:30:31 +0900

    불필요한 파일 삭제

--- server.js
+++ server.js
@@ -1,37 +1,14 @@
-if(process.env.NODE_ENV !== 'prodyction'){
-  require('dotenv').config()
-}
+require('dotenv').config()
 
 const express = require('express')
 const app = express()
 const bcrypt = require('bcrypt')
-const passport = require('passport')
-const flash = require('express-flash')
-const session = require('express-session')
-const methodOverride = require('method-override')
-const initializePassport =  require('./passport-config')
 const jwt = require('jsonwebtoken');
 const { getConfigByIdAndName, getDefaultConfig } = require('./config')
 const { checkUserExistByEmail, users } = require('./user')
 const { execTx, getBalance } = require('./transaction')
 
-initializePassport(
-  passport, 
-  email => users.find(user => user.email === email),
-  id => users.find(user => user.id === id)
-)
-
 app.use(express.json())
-app.use(flash())
-app.use(session({
-  secret: process.env.SESSION_SECRET || 'tmpSecretKey',
-  resave: false,
-  saveUninitialized: false
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(methodOverride('_method'))
-
 
 app.post('/register', async (req, res)=> {
   try{
